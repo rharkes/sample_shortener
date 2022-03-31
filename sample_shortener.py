8008d5129407610d1867a1039a35798586950fea
Rolf Harkes <r.harkes@nki.nl>
2022-03-31 21:33:19 +0200

    Update sample_shortener.py

--- sample_shortener.py
+++ sample_shortener.py
@@ -1,28 +1,38 @@
+"""
+Generates samples for PianOil (https://github.com/nicolasbrailo/PianOli)
+"""
 import soundfile as sf
 from pathlib import Path
 import numpy as np
+from scipy.interpolate import interp1d
 
 notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-start = ['F', '0']
-end = ['C', '7']
-vel = 'Mp'
-pth = Path(r'D:\github_reps\SplendidGrandPiano\Samples')
+start = ['C', '3']
+end = ['B', '4']
+vel = 'vL'
+pth = Path(r'\\samples')  # set the directory here, and set the naming scheme for the filenames on line 22
 
 octave = start[1]
 note = start[0]
 samplerate = 44100
 data = np.zeros((samplerate, 2))
 Path(Path.cwd(), 'samples').mkdir(exist_ok=True)
+i = 0
 while True:
-    file = Path(pth, ''.join([vel, ' ', note, octave, '.flac']))
-    file_out = Path(Path.cwd(), 'samples', file.stem + '.wav')
+    file = Path(pth, ''.join([note, octave, vel, '.flac']))
+    file_out = Path(Path.cwd(), 'samples', f"n{i:02}.wav")
+    i += 1
     if file.exists():
         data, samplerate = sf.read(file)
+        data = data[:, 1]
         print(f"{file.stem} exists")
     else:
         print(f"{file.stem} not exists")
-        samplerate = int(samplerate * 2**(1/12))  # up the previous note by a semitone
-    data_sh = data[0:samplerate, 1]
+        org_x = np.arange(0, data.shape[0], 1)
+        func = interp1d(org_x, data, 'cubic')
+        interp_x = np.arange(0, data.shape[0] - 1, (2 ** (1 / 12)))
+        data = func(interp_x)
+    data_sh = data[0:samplerate]
     fadeout = np.linspace(1, 0, samplerate)
     data_sh *= fadeout
     sf.write(file_out, data_sh, samplerate)
@@ -30,4 +40,4 @@ while True:
         break
     if note == 'B':
         octave = str(int(octave) + 1)
-    note = notes[(notes.index(note)+1) % len(notes)]
+    note = notes[(notes.index(note) + 1) % len(notes)]
